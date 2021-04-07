f0f48e2ab43bbd5ae21b01e6acbc1e5afeeaf56a
Calvin <calvinchan87@gmail.com>
2021-04-07 23:19:06 +0000

    Connection Event Handler, Sending a Name

--- client.js
+++ client.js
@@ -11,6 +11,14 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: CTC');
+  });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
