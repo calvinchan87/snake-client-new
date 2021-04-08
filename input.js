2ba9a5c97ac7fbd2eda79ed851acfcd3d5d42670
Calvin <calvinchan87@gmail.com>
2021-04-08 03:21:38 +0000

    WASD Movement - Sending Data From Input Module

--- input.js
+++ input.js
@@ -1,13 +1,29 @@
+// Stores the active TCP connection object.
+let connection;
+
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
- const setupInput = function() {
+ const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', function handleUserInput(key) {
+    if (key === 'w') {
+      conn.write('Move: up');
+    }
+    if (key === 'a') {
+      conn.write('Move: left');
+    }
+    if (key === 's') {
+      conn.write('Move: down');
+    }
+    if (key === 'd') {
+      conn.write('Move: right');
+    }
     if (key === '\u0003') {
       process.exit();
     }
