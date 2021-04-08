66e6b1b6911db8f0541ed57a642a4dd46755d9c2
Calvin <calvinchan87@gmail.com>
2021-04-08 03:09:27 +0000

    Setup stdin - Handling CTRL + C User Input

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const connect = require('./client');
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+ const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', function handleUserInput(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+}
+
+setupInput();
 console.log('Connecting ...');
 connect();
